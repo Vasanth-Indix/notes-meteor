8d71d5e1c65fa2dcde4e52852b3a52721c459096
Vasanth-Indix <vasanthgopal@Vasanths-MacBook-Air.local>
2015-03-07 12:37:31 +0530

    Notes updating and deletion

--- notes/notes.js
+++ notes/notes.js
@@ -4,7 +4,36 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     notes: function(){
-      return Notes.find({});
+      return Notes.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-note": function (event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      Notes.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
+
+  Template.note.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Notes.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Notes.remove(this._id);
     }
   });
 }
